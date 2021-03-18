30c735e97cb4d15dbd6c19f2160e711a85650cbd
Will Papp <wpapp90@gmail.com>
2021-03-18 17:39:14 -0500

    set up html routes, can now get the html pages to show up in Insomnia using PORT 3000

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+//Dependencies
 const express = require("express");
 
-// const app = express();
-
+//Sets up express app
+const app = express();
 const PORT = 3000;
 
+//Sets up Express app to handle the data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+//Route to send html pages
+require('./routes/htmlRoutes')(app);
 
 
 
+
+//Listen to PORT
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`)
 })
